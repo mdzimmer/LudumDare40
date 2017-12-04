4ca4497c2ff452ef23da2696b5a24fe616b8f1fc
Zachary Petersen <zacharypetersen1@gmail.com>
2017-12-03 21:08:40 -0800

    Add cog prefab

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    float rate;
+
+    void Start()
+    {
+        rate = Random.Range(250, 400);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.right * Time.deltaTime * rate);
+	}
+}
